fe1ab1aa7835fa9586e101b4a91a24180fa4008d
LX199606 <1172241255@qq.com>
2020-08-10 22:33:52 -0400

    lidar_data_process

--- LIDAR/Lidar.py
+++ LIDAR/Lidar.py
@@ -1,35 +1,95 @@
 import os
+import cv2
 import time
 import numpy as np
-from Lidar.function import lidar2image
-import argparse
+from function import find_files, cal_proj_matrix, load_img, load_lidar, project_lidar2img, generate_FV
+from tqdm import tqdm
+import argparse as args
 
 
 def main():
     # parsing arguments
-    argparser = argparse.ArgumentParser(description='Lidar2image')
-    argparser.add_argument('--data_path', required=True, help='path to the data dir. See README for detail.')
-    argparser.add_argument('--out_path', required=True, help='path of output')
-    argparser.add_argument('--type', required=True, help='Type of Lidar representation.choose: proj, map, etc')
-    args = argparser.parse_args()
-
+    # argparser = argparse.ArgumentParser(description='Lidar2image')
+    # argparser.add_argument('--data_path', required=True, help='path to the data dir. See README for detail.')
+    # argparser.add_argument('--out_path_Lidar2img', required=True, help='path of image output')
+    # argparser.add_argument('--out_path_Lidar2FV', required=True, help='path of frontview output')
+    # argparser.add_argument('--type', required=True, help='Type of Lidar representation.choose: proj, FV, etc')
+    # args = argparser.parse_args()
+    args.data_path='/home/lx/program/LORD-Lidar-Orientated-Road-Detection/dataset/'
+    args.out_path_Lidar2img='/home/lx/program/LORD-Lidar-Orientated-Road-Detection/outputs/Lidar2img/'
+    args.out_path_Lidar2FV = '/home/lx/program/LORD-Lidar-Orientated-Road-Detection/outputs/Lidar2FV/'
+    args.type='FV'
     print('- Original Lidar Sources are from: %s' % args.data_path)
-    print('- Lidar2image results will be saved at: %s' % args.out_path)
+    print('- Lidar2image results will be saved at: %s' % args.out_path_Lidar2img)
+    print('- Lidar2FV results will be saved at: %s' % args.out_path_Lidar2FV)
+    # Calib File
+    CALIB = args.data_path+"training/calib/"
+    ################# PARAMETER ####################
+    CAM_ID = 2
+    flag = 'xyz'
+    # Source File
+    IMG_PATH = args.data_path+"training/image_2/"
+    LIDAR_PATH = args.data_path+"training/velodyne/"
+    # Save File
+    SIMG_PATH = args.out_path_Lidar2img
+    #-----------------------------------IMG_Process------------------------------------------------------------
+    # Batch Process
+    time_cost = []
+    for img_path in tqdm(find_files(IMG_PATH, '*.png')):
+        _, img_name = os.path.split(img_path)
+        pc_path = LIDAR_PATH + img_name[:-4] + '.bin'
+        calib_path = CALIB + img_name[:-4] + '.txt'
+        # print ("Working on", img_name[:-4])
+        start_time = time.time()
 
-    names_sample = []
-    for file in os.listdir(os.path.join(args.data_path,'training','velodyne')):
-        if file[-4:] == '.bin':
-            names_sample.append(file[:-4])
-    # loop each sample
-    for i, sample_name in enumerate(names_sample):
-        try:
-            print('==> running sample ' + sample_name + ', index=%d' % i)
-            # type = 'proj' (lidar projection) / type = 'map' (lidar mapping)
-            done = lidar2image(sample_name=sample_name,data_path=args.data_path,out_path=args.out_path,type=args.type)
-            if done:
-                print('==> finish sample ' + sample_name + ', index=%d' % i)
-        except:
-            print('all samples were processed')
+        # Load img & pc
+        img = load_img(img_path)
+        pc = load_lidar(pc_path)
+        pc_distance = pc.copy()
+        if flag == 'x':
+            pass
+        elif flag == 'xy':
+            pc_distance[..., 0] = np.sqrt(pc[..., 0] ** 2 + pc[..., 1] ** 2)
+        elif flag == 'xyz':
+            pc_distance[..., 0] = np.sqrt(pc[..., 0] ** 2 + pc[..., 1] ** 2 + pc[..., 2] ** 2)
+
+        # Project & Generate Image & Save
+        p_matrix = cal_proj_matrix(calib_path, CAM_ID)
+        points = project_lidar2img(img, pc_distance, p_matrix)
+
+        pcimg = img.copy()
+        depth_max = np.max(pc_distance[:, 0])
+        for idx, i in enumerate(points):
+            color = int((pc_distance[idx, 0] / depth_max) * 255)
+            # cv2.rectangle(pcimg, (int(i[0]-1),int(i[1]-1)), (int(i[0]+1),int(i[1]+1)), (0, 0, color), -1)
+            cv2.circle(pcimg, (int(i[0]), int(i[1])), 1, (0, 0, color), -1)
+        cv2.imwrite(SIMG_PATH + img_name, pcimg)
+        end_time = time.time()
+        time_cost.append(end_time - start_time)
+
+    print("Mean_time_cost:", np.mean(time_cost))
+    cv2.destroyAllWindows()
+    # -----------------------FV Process---------------------------------------------------------------------------
+    for img_path in tqdm(find_files(IMG_PATH, '*.png')):
+        _, img_name = os.path.split(img_path)
+        pc_path = LIDAR_PATH + img_name[:-4] + '.bin'
+        # print ("Working on", img_name[:-4])
+        start_time = time.time()
+
+        HRES = 0.35  # horizontal resolution (assuming 20Hz setting)
+        VRES = 0.4  # vertical res
+        VFOV = (-24.9, 2.0)  # Field of view (-ve, +ve) along vertical axis
+        val = 'depth'
+        cmap = 'jet'
+        savepath = args.out_path_Lidar2FV + img_name
+        Y_FUDGE = 5  # y fudge factor for velodyne HDL 64E
+        path_temp = args.data_path + 'training/velodyne/' + img_name[:-4] + '.bin'
+        pointcloud = np.fromfile(path_temp, dtype=np.float32, count=-1).reshape([-1, 4])
+        generate_FV(pointcloud, VRES, HRES, VFOV, val, cmap, savepath, Y_FUDGE)
+        end_time = time.time()
+        time_cost.append(end_time - start_time)
+    print("Mean_time_cost:", np.mean(time_cost))
+    print('all samples were processed')
 
 if __name__ == "__main__":
     main()
---
+++ LIDAR/readme.txt
@@ -0,0 +1,3 @@
+Run Lidar.py to transform from lidar data to images and frontview images.
+
+outputs are saved in /LORD-Lidar-Orientated-Road-Detection/outputs/Lidar2FV and /LORD-Lidar-Orientated-Road-Detection/outputs/Lidar2img
